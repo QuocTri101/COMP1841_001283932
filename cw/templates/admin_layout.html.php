bd68e976913e7491dba04800fbda4e9a46c0b00a
Quốc Trí <122670387+QuocTri101@users.noreply.github.com>
2025-04-30 15:32:51 +0700

    admin_layout.html.php

--- cw/templates/admin_layout.html.php
+++ cw/templates/admin_layout.html.php
@@ -5,7 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/style.css">
     <title><?=$title?></title>
-    <script src="../includes/HTMLFunctions.js"></script>
 
 </head>
 
